bcc3837fccee51d6f207049994993cd31a1f948a
Jinwon-Dev <yjw9424@naver.com>
2023-11-04 00:12:09 +0900

    feat: Health Endpoint 커스텀 변경

---
+++ actuator/src/main/java/io/wisoft/actuator/health/MyCustomHealthIndicator.java
@@ -0,0 +1,29 @@
+package io.wisoft.actuator.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyCustomHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() { // 입맛에 맞게 변경해서 사용하자
+
+        if (isStatusUp()) {
+            return Health.up()
+                    .withDetail("key1", "value1")
+                    .withDetail("key2", "value2")
+                    .build();
+        }
+
+        return Health.down()
+                .withDetail("key3", "value3")
+                .withDetail("key4", "value4")
+                .build();
+    }
+
+    boolean isStatusUp() {
+        return System.currentTimeMillis() % 2 == 0;
+    }
+}
